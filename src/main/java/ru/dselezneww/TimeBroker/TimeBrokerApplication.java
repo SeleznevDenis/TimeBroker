9cd0a209700bfb9914cd0b42a0452414b8c03d5d
Denis Seleznev <d.selezneww@yandex.ru>
2023-04-28 03:03:14 +0500

    Project structure

---
+++ src/main/java/ru/dselezneww/TimeBroker/TimeBrokerApplication.java
@@ -0,0 +1,13 @@
+package ru.dselezneww.TimeBroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeBrokerApplication.class, args);
+	}
+
+}
