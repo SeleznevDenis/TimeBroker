4603a4b05ffd7eb234c2443d5e8cf40f52dfcb6a
Denis Seleznev <d.selezneww@yandex.ru>
2023-04-28 04:09:14 +0500

    Echo bot

---
+++ src/main/java/ru/dselezneww/TimeBroker/configuration/AppConfig.java
@@ -0,0 +1,16 @@
+package ru.dselezneww.TimeBroker.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    TelegramBotsApi telegramBotsApi() throws TelegramApiException {
+        return new TelegramBotsApi(DefaultBotSession.class);
+    }
+}
---
+++ src/main/java/ru/dselezneww/TimeBroker/handler/TelegramBot.java
@@ -0,0 +1,58 @@
+package ru.dselezneww.TimeBroker.handler;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.CopyMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final String name;
+    private final TelegramBotsApi telegramBotApi;
+
+    public TelegramBot(
+            @Value("bot.name")String name,
+            @Value("bot.token")String token,
+            TelegramBotsApi telegramBotsApi) {
+        super(token);
+        this.name = name;
+        this.telegramBotApi = telegramBotsApi;
+    }
+
+    @PostConstruct
+    public void init() throws TelegramApiException {
+        this.telegramBotApi.registerBot(this);
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var msg = update.getMessage();
+        var user = msg.getFrom();
+        var id = user.getId();
+        copyMessage(id, msg.getMessageId());
+    }
+
+    public void copyMessage(Long who, Integer msgId){
+        CopyMessage cm = CopyMessage.builder()
+                .fromChatId(who.toString())  //We copy from the user
+                .chatId(who.toString())      //And send it back to him
+                .messageId(msgId)            //Specifying what message
+                .build();
+        try {
+            execute(cm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return this.name;
+    }
+}
